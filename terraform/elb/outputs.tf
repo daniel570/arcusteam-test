2fea7cb63dd160b059030317d7ff021ee4bb9935
Daniel Harsheffer <danielharsheffer@gmail.com>
2021-01-09 20:25:54 -0500

    minor fixes

--- terraform/elb/outputs.tf
+++ terraform/elb/outputs.tf
@@ -1,3 +1,11 @@
-output "address" {
+output "elb_address" {
   value = aws_elb.web.dns_name
+}
+
+output "instance1-ip" {
+ value = aws_instance.web.public_ip
+}
+
+output "instance2-ip" {
+ value = aws_instance.web2.public_ip
 }
